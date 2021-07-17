4330f5b4176123925ca33cac09da5b56cac9fc44
Carlos Urquiza <webquiza@gmail.com>
2021-07-17 15:57:01 -0700

    add Pizza contructor for toppings and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 // Business logic
 
+function Pizza(toppings) {
+  this.toppings = toppings;
+  this.price = 0;
+}
+
+Pizza.prototype.chosenToppings = function() {
+  this.price = this.price + (this.toppings.length * 1);
+  return this.price;
+};
 
 // User interface logic 
 
 $(function() {
-  $("").submit(function(e) {
+  $("form#pizza_order").submit(function(e) {
     e.preventDefault();
 
+    let toppingSelected = $("input:checkbox[name=toppings]:checked").map(function() {
+      return this.value;
+    });
+  
+    let guestPizza = new Pizza(toppingSelected);
+  
+    guestPizza.chosenToppings();
+  
+    const answer = guestPizza.price;
+  
+    $("#answer").show();
+    $("#output").text(answer);
                                     
   });
 });
