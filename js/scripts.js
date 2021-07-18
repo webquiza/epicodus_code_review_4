7eeba8312321560139472b559050f15bd7b33477
Carlos Urquiza <webquiza@gmail.com>
2021-07-17 18:09:04 -0700

    add chosenSize prototype and update UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 // Business logic
 
-function Pizza(toppings) {
+function Pizza(toppings, size) {
   this.toppings = toppings;
+  this.size = size;
   this.price = 0;
 }
 
 Pizza.prototype.chosenToppings = function() {
   this.price = this.price + (this.toppings.length * 1);
   return this.price;
+};
+
+Pizza.prototype.chosenSize = function() {
+  if (this.size === "small") {
+    this.price += 10;
+  } else if (this.size === "medium") {
+    this.price += 15;
+  } else if (this.size === "large") {
+    this.price += 20;
+  }
+   return this.price;
 };
 
 // User interface logic 
@@ -19,10 +31,13 @@ $(function() {
     let toppingSelected = $("input:checkbox[name=toppings]:checked").map(function() {
       return this.value;
     });
+
+    let sizeSelected = $("input:radio[name=size]:checked").val();
   
-    let guestPizza = new Pizza(toppingSelected);
+    let guestPizza = new Pizza(toppingSelected, sizeSelected);
   
     guestPizza.chosenToppings();
+    guestPizza.chosenSize();
   
     const answer = guestPizza.price;
   
